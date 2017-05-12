43d585d13483277e5dfe391b0e417fb5fff119d3
leeya018 <leeyahav018@gmail.com>
2017-05-12 17:52:16 +0300

    in show friend we changing the url ending according to the chosing friend

--- home/myFriends/index.php
+++ home/myFriends/index.php
@@ -13,14 +13,29 @@
 		<ul>
 			<?php 
 			$str = "";
+			$strHref = "";
 			foreach ($friendRows as $key => $value) { ?>
 			<?php foreach ($value as $key1 => $value1) { 
-				$str = $str . " " . $value1;
+				$str = $str . $value1 . " ";
 				?>
 				<?php
 			} ?>
 			<li>
-			<a href=""><?php echo $str; ?></a>
+				<?php 
+				$strHref = str_replace(" ","_",$str); // replace the " " char with "_" char for the url
+				$len = strlen($strHref);//return the $strHref length
+				$strHref = substr($strHref,0,$len-1); // cut the last "_" charchter fromthe string
+				?>
+				<?php 
+				echo '<a href="' . $strHref . '">'
+				// strHref we need to see it in the url 
+				?>
+				<?php echo $str ?>
+				<!-- show the str as somthing to click on  -->
+				<?php 
+				'</a>';
+
+				?>
 			</li>
 			<?php 
 			$str = "";
